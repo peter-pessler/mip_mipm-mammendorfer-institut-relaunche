c6bd0cee03b45d717459c7829e876c0d8047b1f0
Peter Peßler <peter@wunderlabs.io>
2023-12-18 14:22:43 +0100

    Zwischenstand 33

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,6 +4,36 @@
 // Ausführen, wenn Website geladen wurde
 $(document).ready(function() {
 
+    /* *********************************************************************************************************************** */
+
+    // Der Button wird mit JavaScript erzeugt und vor dem Ende des body eingebunden.
+    var back_to_top_button = ['<a href="#top" class="back-to-top"></a>'].join("");
+    $("body").append(back_to_top_button)
+
+    // Der Button wird ausgeblendet
+    $(".back-to-top").hide();
+
+    // Funktion für das Scroll-Verhalten
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 100) { // Wenn 100 Pixel gescrolled wurde
+                $('.back-to-top').fadeIn();
+            } else {
+                $('.back-to-top').fadeOut();
+            }
+        });
+
+        $('.back-to-top').click(function () { // Klick auf den Button
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+    });
+
+
+    /* *********************************************************************************************************************** */
+
     /**
      * Accordion
      * @type {jQuery|HTMLElement|*}
